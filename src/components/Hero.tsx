d320b84017681ec22508728a531c938701ee4d9d
Seraph <seraph.axl@gmail.com>
2025-02-16 21:16:40 -0600

    Sombra y separacion de titulo principal en la seccion hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,10 +27,13 @@ const Hero = () => {
         <ParallaxBannerLayer speed={-10} className='absolute inset-0 flex items-center justify-center'>
           <div className='container'>
             <div className='max-w-4xl mx-auto text-center'>
-              <h1 className='text-4xl md:text-6xl font-bold text-white mb-6 leading-tight animate-slide-up'>
+              <h1
+                className='text-4xl md:text-6xl md:leading-[4.1rem]  font-bold text-white mb-6 animate-slide-up drop-shadow-2xl'
+                style={{ animationDelay: '0s' }}
+              >
                 Te ayudamos a conseguir el impulso que necesitas
               </h1>
-              <p className='text-lg md:text-xl text-gray-100 mb-8 animate-slide-up' style={{ animationDelay: '0.2s' }}>
+              <p className='text-lg md:text-2xl text-gray-100 mb-8 animate-slide-up text-balance' style={{ animationDelay: '0.2s' }}>
                 Ofrecemos soluciones financieras personalizadas para hacer realidad tus proyectos sin importar tu historial crediticio.
               </p>
               <Link
